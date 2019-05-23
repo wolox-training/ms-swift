e4527637c8d8351eaae2916ca6efab25dfe77158
matiasschwalb <matiasdavidschwalb@Woloxs-Mac-mini-2.local>
2019-05-23 12:02:43 -0300

    Adjusted button and label layouts for smaller devices

--- WBooks/LogInView.swift
+++ WBooks/LogInView.swift
@@ -30,9 +30,13 @@ private extension LogInView {
     }
 
     func setButtonView() {
-        buttonLogIn.layer.cornerRadius = 50
+        // 19 Works for iPhone Xr (896 pts high), factor it by the height of the actual screen size relation for smaller devices
+        let buttonFontSize: CGFloat = 19 * (UIScreen.main.bounds.height / 896)
+        // 45 Works for iPhone Xr (896 pts high), factor it by the height of the actual screen size relation for smaller devices
+        buttonLogIn.layer.cornerRadius = 45 * (UIScreen.main.bounds.height / 896)
         buttonLogIn.clipsToBounds = true
         buttonLogIn.layer.borderColor = UIColor.white.cgColor
         buttonLogIn.layer.borderWidth = 4
+        buttonLogIn.titleLabel?.font = UIFont.boldSystemFont(ofSize: buttonFontSize)
     }
 }
