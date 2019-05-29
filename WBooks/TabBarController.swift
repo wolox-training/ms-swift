ab7d6a51a9de745ad6820b1f77f4f4111f2e4037
matiasschwalb <matias.schwalb@wolox.com.ar>
2019-05-29 16:20:57 -0300

    changes in pull request

--- WBooks/TabBarController.swift
+++ WBooks/TabBarController.swift
@@ -15,15 +15,12 @@ class TabBarController: UITabBarController {
         
         let home = NavigationController(rootViewController: LibraryViewController())
         home.setupNav()
-   //    let home = LibraryViewController()
         home.tabBarItem = UITabBarItem()
         home.tabBarItem.title = "TAB_LIBRARY_TITLE".localized()
         home.tabBarItem.image = UIImage(named: "ic_library")
         home.tabBarItem.tag = 0
-        print(home)
         
         let wishList = WishListViewController()   // Not developed yet
-//        let wishList = LogInViewController()    // Using LogInViewController for debugging until WishList is developed
         wishList.tabBarItem = UITabBarItem()
         wishList.tabBarItem.title = "TAB_WISHLIST_TITLE".localized()
         wishList.tabBarItem.image = UIImage(named: "ic_wishlist")
