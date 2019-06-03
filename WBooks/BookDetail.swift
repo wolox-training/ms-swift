30ede858614614e69155ce938103d115a5d420b3
matiasschwalb <matiasdavidschwalb@Woloxs-Mac-mini-2.local>
2019-06-03 10:35:26 -0300

    adjusted button height

--- WBooks/BookDetail.swift
+++ WBooks/BookDetail.swift
@@ -42,12 +42,14 @@ class BookDetail: UIView, NibLoadable {
         addToWishlistButton.layer.borderWidth = 4
         addToWishlistButton.layer.cornerRadius = 30 * (UIScreen.main.bounds.height / 896)
         addToWishlistButton.setTitle("DETAIL_WISHLIST_BUTTON_TITLE".localized(), for: .normal)
+        addToWishlistButton.heightAnchor.constraint(equalToConstant: 53.0).isActive = true
         
         rentButton.clipsToBounds = true
         rentButton.layer.borderColor = UIColor.red.cgColor
         rentButton.layer.borderWidth = 4
         rentButton.layer.cornerRadius = 30 * (UIScreen.main.bounds.height / 896)
         rentButton.setTitle("DETAIL_RENT_BUTTON_TITLE".localized(), for: .normal)
+        rentButton.heightAnchor.constraint(equalToConstant: 53.0).isActive = true
         
        // clipsToBounds = true
        // frame = CGRect(origin: CGPoint(x: 80, y: 100), size: self.frame.size)
