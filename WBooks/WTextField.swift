a136bdfb53eaeba32c195ed9c1d3cf37e5fa986c
matiasschwalb <matiasdavidschwalb@Woloxs-Mac-mini-5.local>
2019-06-14 09:50:04 -0300

    fixed image resizing

---
+++ WBooks/WTextField.swift
@@ -0,0 +1,21 @@
+//
+//  WTextField.swift
+//  WBooks
+//
+//  Created by Matías David Schwalb on 14/06/2019.
+//  Copyright © 2019 Wolox. All rights reserved.
+//
+
+import UIKit
+
+class WTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
